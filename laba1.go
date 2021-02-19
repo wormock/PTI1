96c8c3efff1d70b1e7df8956275351d80b468d0a
Евгений Волков <evgenijvolkov@MacBook-Pro-Evgenij.local>
2021-02-19 14:43:39 +0300

    add func

--- laba1.go
+++ laba1.go
@@ -16,6 +16,8 @@ func (s *Realiz) Set() {
 	s.R = rand.Float64()
 	s.X = 1 / k * math.Log((1+s.R)/(1-s.R))
 }
+
+//Заполнение массива
 func SetArr(n int) []Realiz {
 	var array []Realiz
 
@@ -26,8 +28,35 @@ func SetArr(n int) []Realiz {
 	}
 	return array
 }
+
+// Получение оценки матиматического ожидания
+func GetOcMatOj(arr []Realiz) float64 {
+	s := 0.0
+	for i := 0; i < len(arr); i++ {
+		s += arr[i].R
+	}
+	s = 1.0 / float64(len(arr)) * s
+	return s
+}
+
+// Получение оценки дисперсии
+func GetOcDisp(arr []Realiz, mj float64) float64 {
+	s := 0.0
+	for i := 0; i < len(arr); i++ {
+		s += math.Pow((arr[i].R - mj), 2.0)
+	}
+	s = 1.0 / float64(len(arr)) * s
+	return s
+}
+
 func main() {
 	var arr []Realiz
 	arr = SetArr(10)
+	mtj := GetOcMatOj(arr)
+	disp := GetOcDisp(arr, mtj)
+	otkl := math.Sqrt(disp)
 	fmt.Print(arr)
+	fmt.Printf("\nMatOj = %g", mtj)
+	fmt.Printf("\nDisp = %g", disp)
+	fmt.Printf("\nSrKvadOtkl = %g", otkl)
 }
